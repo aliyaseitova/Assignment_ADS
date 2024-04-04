1b2a88c753f9a815bd6c79db7e57d572db7b0e44
aliyaseitova <153705534+aliyaseitova@users.noreply.github.com>
2024-04-04 11:21:50 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class MinElementFinder(){
+    public static int findMin(int[] arr){
+        if (arr == null || arr.length==0){
+            throw new IllegalArgumentException("Array must not be empty or null");
+        }
+
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++){
+            if (arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
